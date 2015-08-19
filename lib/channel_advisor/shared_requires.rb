c3451c1ee1d657b46247bf26e53ffa9194169d76
Kyle Shannon <ktshannon@gmail.com>
2015-08-19 17:47:54 -0400

    Try soap4r-ng gem

--- lib/channel_advisor/shared_requires.rb
+++ lib/channel_advisor/shared_requires.rb
@@ -1,5 +1,5 @@
 require 'rubygems'
-gem 'soap4r-ruby1.9'
+gem 'soap4r-ng', :git=>'https://github.com/rubyjedi/soap4r.git', :branch=>'master'
 require 'xsd/qname'
 require 'xsd/mapping'
 require 'soap/mapping'
